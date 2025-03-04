59a234769d69642c762fb1e6ff3a8a0539996100
Du Wei <duweigg.gmail.com>
2025-03-04 09:13:43 +0800

    [Sign in & Sign up] update styles

---
+++ e-library-fe/src/app/signin/page.js
@@ -0,0 +1,60 @@
+"use client"
+import { getUserInfo, Signin } from "@/api/user";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const router = useRouter()
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "60%",
+        width: 500,
+        border: "solid 1px light-grey",
+        backgroundColor: "rgba(0,0,0,0.4)",
+        color: "white"
+      }}>
+        <div style={{ fontSize: 40, fontWeight: "700", marginTop: "20%", marginBottom: "5%" }}>Sign In</div>
+        <div style={{ width: "50%" }}>
+          <div style={{ fontSize: 20 }}>Username</div>
+          <input
+            style={{ backgroundColor: "rgba(255,255,255,0.1)", width: "100%" }}
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value)
+            }}
+          ></input>
+        </div>
+        <div style={{ height: 30 }}></div>
+        <div style={{ width: "50%" }}>
+          <div style={{ fontSize: 20 }}>Password</div>
+          <input
+            style={{ backgroundColor: "rgba(255,255,255,0.1)", width: "100%" }}
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}></input>
+        </div>
+        <button
+          style={{ marginTop: 80, marginBottom: 10, border: "solid 2px white", padding: "5px 10px 5px 10px", borderRadius: 20 }}
+          onClick={() => {
+            Signin(username, password)
+          }}>Sign In</button>
+        <div style={{ fontSize: 13, color: "grey" }}>Do not have account yet? <span style={{ color: "white" }} onClick={() => { router.push("/signup") }}>Sign Up</span></div>
+
+      </div>
+    </div >
+  );
+}
--- e-library-fe/src/app/signup/page.js
+++ e-library-fe/src/app/signup/page.js
@@ -6,10 +6,10 @@ import { useState } from "react";
 
 export default function SignUp() {
   const router = useRouter()
-  const [username,setUsername]=useState("")
-  const [password,setPassword]=useState("")
-  const [confirmPassword,setConfirmPassword]=useState("")
-  const [nickname,setNickname]=useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [nickname, setNickname] = useState("")
   return (
     <div
       style={{
@@ -18,43 +18,69 @@ export default function SignUp() {
         justifyContent: "center",
         alignItems: "center",
       }}>
-      <div style={{ height: "80%", width: 500, border: "solid 1px light-grey", backgroundColor: "rgba(0,0,0,0.4)", color:"white" }}>
-        <div>Sign Up</div>
-        <div>Username</div>
-        <input
-        style={{backgroundColor:"rgba(255,255,255,0.1)"}}
-          value = {username}
-          onChange={(e)=>{
-            setUsername(e.target.value)
-          }}
-        ></input>
-        <div>Password</div>
-        <input
-        style={{backgroundColor:"rgba(255,255,255,0.1)"}}
-          value = {password}
-          onChange={(e)=>{
-            setPassword(e.target.value)
-          }}></input>
-        <div>Confirm Password</div>
-        <input
-        style={{backgroundColor:"rgba(255,255,255,0.1)"}}
-          value = {confirmPassword}
-          onChange={(e)=>{
-            setConfirmPassword(e.target.value)
-          }}></input>
-        <div>Nickname</div>
-        <input
-        style={{backgroundColor:"rgba(255,255,255,0.1)"}}
-          value = {nickname}
-          onChange={(e)=>{
-            setNickname(e.target.value)
-          }}></input>
-        <div>
-          <button onClick={()=>{
-            Register(username, password, nickname)
-          }}>Sign Up</button>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "60%",
+        width: 500,
+        border: "solid 1px light-grey",
+        backgroundColor: "rgba(0,0,0,0.4)",
+        color: "white"
+      }}>
+        <div style={{ fontSize: 40, fontWeight: "700", marginTop: "20%", marginBottom: "5%" }}>Sign Up</div>
+        <div style={{ width: "50%" }}>
+          <div style={{ fontSize: 20 }}>Username</div>
+          <input
+            style={{ backgroundColor: "rgba(255,255,255,0.1)", width: "100%" }}
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value)
+            }}
+          ></input>
         </div>
-        <div>Already have an account? <span onClick={() => { router.push("/login") }}>Sign In</span></div>
+        <div style={{ height: 15 }}></div>
+        <div style={{ width: "50%" }}>
+          <div>Password</div>
+          <input style={{ backgroundColor: "rgba(255,255,255,0.1)", width: "100%" }}
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}></input>
+        </div>
+        <div style={{ height: 15 }}></div>
+        <div style={{ width: "50%" }}>
+          <div>Confirm Password</div>
+          <input style={{ backgroundColor: "rgba(255,255,255,0.1)", width: "100%" }}
+            value={confirmPassword}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value)
+            }}></input>
+        </div>
+        <div style={{ height: 15 }}></div>
+        <div style={{ width: "50%" }}>
+          <div>Nickname</div>
+          <input style={{ backgroundColor: "rgba(255,255,255,0.1)", width: "100%" }}
+            value={nickname}
+            onChange={(e) => {
+              setNickname(e.target.value)
+            }}></input>
+        </div>
+        <div>
+          <button style={{ marginTop: 40, marginBottom: 10, border: "solid 2px white", padding: "5px 10px 5px 10px", borderRadius: 20 }}
+            onClick={() => {
+              Register(username, password, nickname)
+            }}>Sign Up</button>
+        </div>
+        <div
+          style={{ fontSize: 13, color: "grey" }}>
+          Already have an account?
+          <span
+            style={{ color: "white" }}
+            onClick={() => { router.push("/signin") }}>
+            Sign In
+          </span>
+        </div>
 
       </div>
     </div >
